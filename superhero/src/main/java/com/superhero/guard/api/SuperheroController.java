ac4a295ec249b9216286c9859bfcc340cf37ff99
nagsen1 <prince.nagsen@gmail.com>
2024-06-11 21:41:50 +0200

    refactor:updated the controller methods and added exception handling

--- superhero/src/main/java/com/superhero/guard/api/SuperheroController.java
+++ superhero/src/main/java/com/superhero/guard/api/SuperheroController.java
@@ -1,10 +1,17 @@
 package com.superhero.guard.api;
 
 import com.superhero.guard.entity.Superhero;
+import com.superhero.guard.services.PowerService;
 import com.superhero.guard.services.SuperheroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,20 +28,25 @@ public class SuperheroController {
     }
 
     @PostMapping
-    public ResponseEntity<Superhero> save(@RequestBody Superhero entity) {
-        Superhero savedEntity = superheroService.save(entity);
-        return ResponseEntity.ok(savedEntity);
+    public ResponseEntity<Superhero> createSuperhero(@RequestBody Superhero entity) {
+        try {
+
+            Superhero savedEntity = superheroService.save(entity);
+            return ResponseEntity.ok(savedEntity);
+        }catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Superhero> findById(@PathVariable Long id) {
+    public ResponseEntity<Superhero> getSuperheroById(@PathVariable Long id) {
         Optional<Superhero> entity = superheroService.findById(id);
         return entity.map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping
-    public ResponseEntity<List<Superhero>> findAll() {
+    public ResponseEntity<List<Superhero>> getAllSuperheros() {
         List<Superhero> entities = superheroService.findAll();
         return ResponseEntity.ok(entities);
     }
